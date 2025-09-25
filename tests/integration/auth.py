3eaabb84466609b3c3f68d802396185a6f63f3cb
bryandevuono <bryandevuono@gmail.com>
2025-09-25 21:17:19 +0200

    branch name change

--- tests/integration/auth.py
+++ tests/integration/auth.py
@@ -1,6 +1,6 @@
 import requests
 
-BASE_URL = "http://localhost:8000"  
+BASE_URL = "http://localhost:8000"
 
 # happy flow 
 def test_create_user():
@@ -16,3 +16,14 @@ def test_login():
     assert response.status_code == 200
     
 # sad flow
+def test_create_user_null_name():
+    payload = {"username": "", "password": "secret"}
+    response = requests.post(f"{BASE_URL}/register", json=payload)
+
+    assert response.status_code != 200
+    
+def test_create_user_null_password():
+    payload = {"username": "", "password": ""}
+    response = requests.post(f"{BASE_URL}/register", json=payload)
+
+    assert response.status_code != 200
