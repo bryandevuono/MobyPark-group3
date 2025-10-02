b395971cfb94e7d42db6b1672eb49d490439c42d
bryandevuono <114662794+bryandevuono@users.noreply.github.com>
2025-10-02 14:23:18 +0200

    modified tests for authorization

--- tests/integration/auth.py
+++ tests/integration/auth.py
@@ -2,55 +2,59 @@ import requests
 
 BASE_URL = "http://localhost:8000"
 
-# happy flow 
+
+# happy flow
 def test_create_user():
     payload = {
-        "username": "meneer", 
-        "password": "secret"
-        }
+        "username": "johndoe",
+        "email": "johndoe@example.com",
+        "password": "strongpassword123",
+        "name": "John Doe",
+        "phone": "+1234567890",
+        "birth_year": 1990,
+    }
     response = requests.post(f"{BASE_URL}/register", json=payload)
 
     assert response.status_code == 200
 
+
 def test_login():
-    payload = {
-        "username": "meneer", 
-        "password": "secret"
-        }
+    payload = {"username": "johndoe", "password": "strongpassword123"}
     response = requests.post(f"{BASE_URL}/login", json=payload)
-    
+
     assert response.status_code == 200
 
 
 # sad flow
 def test_create_user_null_name():
-    payload = {
-        "username": "", 
-        "password": "secret"
-    }
+    payload = {"username": "", "password": "secret"}
     response = requests.post(f"{BASE_URL}/register", json=payload)
 
     assert response.status_code != 200
-    
+
+
 def test_create_user_null_password():
-    payload = {
-        "username": "", 
-        "password": ""
-    }
+    payload = {"username": "", "password": ""}
     response = requests.post(f"{BASE_URL}/register", json=payload)
 
     assert response.status_code != 200
 
-def wrong_session_token():
-    response=requests.get(f"{BASE_URL}/vehicles", headers={"Authorization":"djnwjn"})
-    
+
+def test_wrong_session_token():
+    response = requests.get(f"{BASE_URL}/vehicles", headers={"Authorization": "Bearer dujshdsj"})
+
     assert response.status_code == 400
 
-def duplicate_name():
+
+def test_duplicate_create_user():
     payload = {
-        "username": "meneer", 
-        "password": "secret"
-        }
-    response = requests.post(f"{BASE_URL}/login", json=payload)
-    
-    assert response.status_code != 200
+        "username": "johndoe",
+        "email": "johndoe@example.com",
+        "password": "strongpassword123",
+        "name": "John Doe",
+        "phone": "+1234567890",
+        "birth_year": 1990,
+    }
+    response = requests.post(f"{BASE_URL}/register", json=payload)
+
+    assert response.status_code == 400
